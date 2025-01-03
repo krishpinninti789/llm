c990a31a29e096b3bc94a72a0ff9b3e776cb865e
krishpinninti789 <krishpinninti789@gmail.com>
2025-01-03 13:26:43 +0530

    vamsi

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='font-serif'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-gray-100">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="bg-[#0c0c0c] flex flex-col items-center justify-center ">
+        <div className=" w-[100svw] h-[100svh] flex flex-col justify-center items-center">
+          <h1 className="z-1 bg-gradient-to-b from-[#f7eee3] to-[#f3bb9d] text-center bg-clip-text text-transparent   text-[6em] font-serif">
+            Super charged <br />
+            <span className="italic">LMS </span>
+            <span className="italic"> with LLM</span>
+          </h1>
+          <div className="z-2">
+            <div className="flex flex-col items-center justify-center  bg-gradient-to-b from-[#1b1b1b] to-[#0c0c0c] inner  rounded-full w-[50rem] h-[50rem]">
+              <div className="bg-gradient-to-b  from-[#ff5e00] to-[#0c0c0c] h-[30rem] w-[30rem] rounded-full blur-3xl "></div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
